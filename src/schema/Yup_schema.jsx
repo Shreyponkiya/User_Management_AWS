d0b3f9df3020092926d867ce6578bf0265a9ffd1
Shrey Ponkiya <shreyponkiya@gmail.com>
2025-03-21 18:14:35 +0530

    first commit

--- src/schema/Yup_schema.jsx
+++ src/schema/Yup_schema.jsx
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 export const Yup_schema = Yup.object({
   email: Yup.string().email().required("Please Fill This Email"),
-  password: Yup.string().min(6).required("Please Fill This password"),
+  password: Yup.string().min(3).required("Please Fill This password"),
   confirm_password: Yup.string()
     .oneOf([Yup.ref("password"), null], "password must match"),
 });
